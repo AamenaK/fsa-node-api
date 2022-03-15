8c01888aa3ad1ae6516a1bcb3953b09d98ec4738
AamenaK <aamenakausar8@gmail.com>
2022-03-15 23:58:44 +0530

    enable cors

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
-    dbConStr: process.env.dbConStr,
-    jwtSecret: process.env.jwtSecret
+    dbConStr: "mongodb+srv://aamena_cluster:mongodbcluster$123@cluster0.j5ebm.mongodb.net/api?retryWrites=true&w=majority",
+    jwtSecret: "secret@123#"
 };
 
 module.exports = config;
